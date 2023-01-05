d61dcd7f9da9071d2482140155566d4a18d1abc7
Klaven Jones <klavensjones@gmail.com>
2023-01-05 18:09:08 -0500

    Created Database Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { typeOrmConfig } from './typeorm/typeorm.config';
-// import { DataSource } from 'typeorm';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    // TypeOrmModule.forRootAsync({
-    //   useFactory: () => ({
-    //     ...typeOrmConfig,
-    //   }),
-    //   dataSourceFactory: async (options) => {
-    //     const dataSource = await new DataSource(options).initialize();
-    //     return dataSource;
-    //   },
-    // }),
-    UserModule,
-  ],
+  imports: [UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
